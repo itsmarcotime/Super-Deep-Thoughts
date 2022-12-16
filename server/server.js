e47b584a1ccd405050ff720cddcad5a02897af9d
Marco <patrickstar1281@yahoo.com>
2022-12-15 23:34:09 -0600

    trying to deploy the app

--- server/server.js
+++ server/server.js
@@ -44,6 +44,12 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 };
 
+if (process.env.NODE_ENV === 'production') {
+  //set static folder up in production
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+};
+
 //call the async function to start the server 
 startApolloServer(typeDefs, resolvers);
 
